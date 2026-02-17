d0068a1e07183d9a63f65516fd8663eb22a798ea
LauraEsmoris <lesmoris0web@gmail.com>
2026-02-17 12:13:34 -0500

    Buys & reviews table

---
+++ src/tables/Buys&Reviews.ts
@@ -0,0 +1,54 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { Product } from "./Product";
+
+
+@Table
+
+export class BuysAndReviews extends Model<BuysAndReviews, IBuysAndReviews> {
+    @Column({
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    })
+    declare id: number;
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    declare userId: number;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false,
+    })
+    declare date: Date;
+
+    @Column({
+        type: DataType.JSON,
+        allowNull: false,
+    })
+    declare products: Product[];
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    declare review: string;
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    declare carNumber: number;
+
+}
+
+export interface IBuysAndReviews {
+    id: number;
+    userId: number;
+    date: Date;
+    products: Product[];
+    review: string;
+    carNumber: number;
+}
