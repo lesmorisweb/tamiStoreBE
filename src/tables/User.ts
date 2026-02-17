3f966f00a1d73c96775bd33a4660dedcd0ffbaf1
LauraEsmoris <lesmoris0web@gmail.com>
2026-02-17 11:58:15 -0500

    user table done

---
+++ src/tables/User.ts
@@ -0,0 +1,54 @@
+import { Model, Table, Column, DataType } from "sequelize-typescript";
+
+@Table
+export class User extends Model<User, IUser> {
+@Column({
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    })
+    declare id: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    declare name: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+        unique: true,
+    })
+    declare email: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    declare country: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    declare phone: string;
+
+    @Column({
+        type: DataType.ENUM("buyer", "seller"),
+        allowNull: false,
+    })
+    declare accountType: AccountType;
+
+}
+
+export interface IUser {
+    id: number;
+    name: string;
+    email: string;
+    country: string;
+    phone: string;
+    accountType: AccountType;
+}
+
+export type AccountType = "buyer" | "seller";
