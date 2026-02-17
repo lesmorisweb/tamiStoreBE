e01f6d024d96fc8b9b95db6d19b837cc4ffc7ab4
LauraEsmoris <lesmoris0web@gmail.com>
2026-02-17 12:26:51 -0500

    car basic table

---
+++ src/tables/Car.ts
@@ -0,0 +1,40 @@
+import { Column, DataType, Model, Table, } from "sequelize-typescript";
+import { Product } from "./Product";
+
+
+@Table
+
+export class BuysAndReviews extends Model<BuysAndReviews, IBuysAndReviews> {
+
+    @Column({
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    })
+    declare id: number;
+
+    @Column({
+        type: DataType.INTEGER,
+        allowNull: false,
+    })
+    declare userId: number;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    declare carName: string;
+
+    @Column({
+        type: DataType.JSON,
+        allowNull: false,
+    })
+    declare products: Product[];
+}
+
+export interface IBuysAndReviews {
+    id: number;
+    userId: number;
+    carName: string;
+    products: Product[];
+}
